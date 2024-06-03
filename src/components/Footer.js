ab20c5578d3b6f97ab33ee0921df47f62179f57a
Danai Boukiou <danaebouki@gmail.com>
2024-06-03 09:30:59 +0200

    added page components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
-import logo from "../images/Asset 14@4x.png"
+import logo from "../images/Asset 7@4x.png"
+import "./footer.css"
 
 export default function Footer() {
     return (
         <footer>
-            <img src={logo} alt="a black-and-white Little Lemon logo" />
-            2024, Little Lemon, All Rights Reserved.
+            <img src={logo} className="logo" alt="a black-and-white Little Lemon logo" />
+            <span>2024, Little Lemon, All Rights Reserved.</span>
         </footer>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import Nav from "./Nav";
 import logo from "../images/Asset 16@4x.png"
+import "./header.css"
 
 function Header() {
     return (
         <header>
-            <img src={logo} alt="a colored Little Lemon logo" />
+            <img src={logo} className="logo-header" alt="a colored Little Lemon logo" />
             <Nav />
         </header>
     );
